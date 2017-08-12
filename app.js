085815e69f9d04a9f963a25e521236158d961372
Dammie2git <addressjustdammi@gmail.com>
2017-08-12 17:03:49 +0100

    Server live

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 var express = require('express')
 var app = express();
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 app.listen(3001, function() {
     console.log('Listening on 3001');
 });
 
 // Mount the public directory at /
-app.use('/', express.static('/public'));
+app.use('/', express.static('./public'));
+console.log('deploy public');
